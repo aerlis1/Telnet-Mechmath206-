5fecba2fd77f10fff5cdc4e1403eed28dfac5b74
Yarus21 <p.yaroslav.s.21@gmail.com>
2016-04-05 02:27:17 +0400

    Update Pipe.cpp

--- Pipe.cpp
+++ Pipe.cpp
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+
 
 int main() 
 {
@@ -13,29 +15,39 @@ int main()
         pid_t pid;
         
         pipe(fdin), pipe(fdout), pipe(fderr);
- //       dup2(fdout[1], 1);
- //       dup2(fderr[1], 2);
-        
+	
         pid = fork();
         
         if(pid == 0)
         {
-           dup2(fdin[0],0);
-           close(fdin[1]);
-   //         close(fdout[0]); close(fderr[0]);
-            execl("/bin/bash","ls", NULL);
+                //исправил перенаправление оставшихся потоков
+                //теперь вроде всё работает
+            dup2(fdin[0], 0);
+	    dup2(1, fdout[1]);
+	    dup2(2, fderr[1]);
+	    
+            close(fdin[1]);
+            close(fdout[0]); close(fderr[0]);
+	    
+            execl("/bin/bash"," ", NULL);
             exit(1);
         }
         else
         {
             close(fdin[0]); 
-   //         close(fdout[1]); close(fderr[1]);
+            close(fdout[1]); close(fderr[1]);
             
-            std::cin>>buf;
-   //         write(fdin[1], buf, sizeof(buf));
-            write(fdin[1],"ls -l\n",7);
+            //не смог сделать так, чтобы выполняло то, что мы кладём в буфер
+            //там видимо какая-то хрень со строками и символьными массивами
+            //так что пока что выполняется только то, что пишем в кавычках вручную
+            
+            //std::cin>>buf;
+
+           //write(fdin[1], buf, sizeof(buf));
+            
+	    write(fdin[1],"ls -l\n",7);
             read(fdout[0], buf, sizeof(buf));
-   //         std::cout<<buf<<"\n";
+            std::cout<<"\nOur result: \n"<<buf<<"\n";
         }
         return 0;
 }
